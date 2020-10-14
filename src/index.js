e00655069b7c092d50f20714c2462aa923818997
100Rubley <toxa_98@inbox.ru>
2020-10-14 20:33:02 +0300

    feat: all test pass

--- src/index.js
+++ src/index.js
@@ -42,34 +42,35 @@ function decode(expr) {
   let exprArray = expr.split('')
   let answer = ''
 
-  for (let i = 0; i < exprArray.length / 10; i++) {
+  for (let i = 0; i < expr.length / 10; i++) {
     let exprArrayCut = exprArray.splice(0, 10)
+    let a = getMorzeCode(exprArrayCut)
 
-    if (getMorzeCode(exprArrayCut) === ' ') {
+    if (a === '') {
       answer += ' '
     } else {
-      answer += MORSE_TABLE[getMorzeCode(exprArray)]
+      answer += MORSE_TABLE[a]
     }
   }
 
   function getMorzeCode(str) { //this function translating a 10 length string into a dash-dot code
     let letterBinaryCode = str
-    let result = ''
+    let result = []
 
     for (let i = str.length - 1; i > 0; i -= 2) {
       if (letterBinaryCode === '**********') return ' '
       if (letterBinaryCode[i] === '0' && letterBinaryCode[i - 1] === '1') {
-        result += '.'
+        result.unshift('.')
         letterBinaryCode.pop()
         letterBinaryCode.pop()
       }
       if (letterBinaryCode[i] === '1' && letterBinaryCode[i - 1] === '1') {
-        result += '-'
+        result.unshift('-')
         letterBinaryCode.pop()
         letterBinaryCode.pop()
       }
     }
-    return result
+    return result.join('')
   }
 
   return answer
